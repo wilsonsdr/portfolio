0472bd537857bc8d1bb99eb10c6a181fbbb647bb
Wilson Reis <wilsonsdr@gmail.com>
2024-02-17 10:49:46 -0300

    Changing the name of the functions

--- src/components/elements/List.tsx
+++ src/components/elements/List.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Color } from "../enum/color";
+import React from 'react';
+import { Color } from '../enum/color';
 
-interface ListProps {
+interface ListContainerProps {
   children: React.ReactNode;
 }
 
-export default function List(props: ListProps) {
-  return <ul className="flex flex-col gap-2">{props.children}</ul>;
+export default function ListContainer(props: ListContainerProps) {
+  return <ul className='flex flex-col gap-2'>{props.children}</ul>;
 }
 
-interface ItemProps {
+interface ListItemProps {
   children: React.ReactNode;
   color: Color;
 }
 
-export function Item(props: ItemProps) {
+export function ListItem(props: ListItemProps) {
   return <li className={props.color}>{props.children}</li>;
 }
