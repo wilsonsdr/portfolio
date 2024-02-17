1850d809b244132108d2dc87aa7e4c640c223267
Wilson Reis <wilsonsdr@gmail.com>
2024-02-17 09:00:05 -0300

    Adjusting the height of the component

--- src/components/elements/Hr.tsx
+++ src/components/elements/Hr.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Hr() {
-  return <span className="h-[0.1px] w-11/12 rounded-md bg-dark-color"></span>;
+  return <span className="h-[1px] w-11/12 rounded-md bg-dark-color"></span>;
 }
