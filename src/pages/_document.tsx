115b1af89939c0d8a2238419cc0b3675b61c19ba
Wilson Reis <wilsonsdr@gmail.com>
2024-02-17 08:49:11 -0300

    Changing body max width

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
+    <Html lang='pt-br'>
       <Head />
-      <body className="mx-auto w-10/12 bg-body-color py-20 max-sm:w-full max-sm:p-4">
+      <body className='mx-auto max-w-7xl bg-body-color px-10 py-20 max-sm:w-full max-sm:p-4'>
         <Main />
         <NextScript />
       </body>
